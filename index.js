cc5b6368307413ce1cd7678b66166115e6975ecb
rajareddykodakandla <rajareddykodakandla@gmail.com>
2025-01-03 19:01:13 -0500

    update

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const { createTodo, updateTodo } = require("./types");
 
 app.use(express.json());
 
@@ -7,11 +8,25 @@ app.get("/", (req, res) => {
   res.send("Hello There");
 });
 
-app.post("/todo", (req, res) => { });
+app.post("/todo", (req, res) => {
+  const parsedData = createTodo.safeParse(req.body);
+  if (!parsedData.success) {
+    return res.status(401).json({
+      msg: "You sent the wrong inputs",
+    });
+  }
+});
 
 app.get("/todos", (req, res) => { });
 
-app.put("/completed", (req, res) => { });
+app.put("/completed", (req, res) => {
+  const parsedData = updateTodo.safeParse(req.body);
+  if (!parsedData.success) {
+    return res.status(401).json({
+      msg: "You sent the wrong inputs",
+    });
+  }
+});
 
 app.use((err, req, res, next) => {
   console.log(err.stack);
